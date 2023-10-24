3144fc38c64b0bcd3a938e79bd2066b62a1e806c
Eri Suzuki <Eri.A.Suzuki@sony.com>
2023-10-24 12:55:17 +0900

    [update] 初期状態に戻した。

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,3 @@
 Rails.application.routes.draw do
-  resources :books
-  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "hello/index" # get "hello/index", to: "hello#index"
-  get "hello/view"
-  get "hello/list"
 end
